dfbfba624c439ad2736df436c8921e7565931fea
andre <andrecabralpires@gmail.com>
2022-06-03 09:22:45 +0100

    added some card

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,4 @@
-# app/controllers/products_controllers.rb
 class ProductsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
-
   def index
     @products = Product.all
   end
